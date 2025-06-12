ec879262095142f650607589a693c747fb899b73
whydumb <108032052+whydumb@users.noreply.github.com>
2025-06-12 11:53:50 +0900

    Update walk_controller_mongodb.hpp

--- controllers/walk_controller_mongodb/walk_controller_mongodb.hpp
+++ controllers/walk_controller_mongodb/walk_controller_mongodb.hpp
@@ -1,43 +1,80 @@
-// Copyright 1996-2020 Cyberbotics Ltd.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// walk_controller_mongodb.hpp
+// MongoDB 연동이 추가된 Walk Controller 헤더
 
-// Description: MongoDB 실시간 명령 처리 컨트롤러 메인 함수
+#ifndef WALK_MONGODB_HPP
+#define WALK_MONGODB_HPP
 
-#include "walk_controller_mongodb.hpp"
+#define NMOTORS 20
 
-#include <cstdlib>
-#include <iostream>
+#include <webots/Robot.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/database.hpp>
+#include <mongocxx/collection.hpp>
+#include <string>
+#include <map>
 
-using namespace webots;
-using namespace std;
-
-int main(int argc, char **argv) {
-    cout << "Starting ROBOTIS OP2 MongoDB Real-Time Controller..." << endl;
-    
-    try {
-        WalkMongoDB *controller = new WalkMongoDB();
-        controller->run();
-        delete controller;
-    }
-    catch (const exception& e) {
-        cerr << "Error: " << e.what() << endl;
-        return EXIT_FAILURE;
-    }
-    catch (...) {
-        cerr << "Unknown error occurred" << endl;
-        return EXIT_FAILURE;
-    }
-    
-    return EXIT_SUCCESS;
+namespace managers {
+  class RobotisOp2MotionManager;
+  class RobotisOp2GaitManager;
 }
+
+namespace webots {
+  class Motor;
+  class PositionSensor;
+  class LED;
+  class Camera;
+  class Accelerometer;
+  class Gyro;
+  class Keyboard;
+  class Speaker;
+}
+
+// 동작 파라미터 구조체
+struct GaitParams {
+  double xAmplitude;  // 전진/후진 (-1.0 ~ 1.0)
+  double aAmplitude;  // 좌우회전 (-0.5 ~ 0.5)
+  bool startGait;     // 걷기 시작 여부
+};
+
+class Walk : public webots::Robot {
+public:
+  Walk();
+  virtual ~Walk();
+  void run();
+  void checkIfFallen();
+
+private:
+  int mTimeStep;
+  bool mIsWalking;
+
+  // 기본 메서드
+  void myStep();
+  void wait(int ms);
+
+  // MongoDB 관련 메서드
+  void initializeMongoDB();
+  void initializeActionMapper();
+  void applyActionFromMongoDB(const std::string& actionStr);
+  void readAndExecuteFromMongoDB();
+  void playbackFromMongoDB();
+
+  // Webots 하드웨어 인터페이스
+  webots::Motor *mMotors[NMOTORS];
+  webots::PositionSensor *mPositionSensors[NMOTORS];
+  webots::Accelerometer *mAccelerometer;
+  webots::Keyboard *mKeyboard;
+
+  // ROBOTIS 관리자
+  managers::RobotisOp2MotionManager *mMotionManager;
+  managers::RobotisOp2GaitManager *mGaitManager;
+
+  // MongoDB 관련
+  mongocxx::client mMongoClient;
+  mongocxx::database mDatabase;
+  mongocxx::collection mCollection;
+
+  // ActionType 매핑
+  std::map<std::string, GaitParams> mActionMap;
+};
+
+#endif
