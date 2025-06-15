915ab9befe1aed3f78a6865f804fb860df499b6d
whydumb <108032052+whydumb@users.noreply.github.com>
2025-06-15 16:00:19 +0900

    Update walk_controller_mongodb.hpp

--- controllers/walk_controller_mongodb/walk_controller_mongodb.hpp
+++ controllers/walk_controller_mongodb/walk_controller_mongodb.hpp
@@ -1,4 +1,4 @@
-// Walk.hpp - 간단한 MongoDB 버전
+// walk_controller_mongodb.hpp - 완전한 MongoDB 버전
 #ifndef WALK_CONTROLLER_MONGODB_HPP
 #define WALK_CONTROLLER_MONGODB_HPP
 
@@ -9,6 +9,8 @@
 #ifdef USE_MONGODB
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
+#include <mongocxx/collection.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
 #include <memory>
 #include <string>
 #endif
@@ -49,8 +51,10 @@ private:
   std::unique_ptr<mongocxx::instance> mMongoInstance;
   std::unique_ptr<mongocxx::client> mMongoClient;
   mongocxx::collection mCollection;
+  bool mMongoConnected;
   int mMongoCheckCounter;
   
+  // MongoDB 관련 메서드
   std::string getMongoAction();
   void executeMongoAction(const std::string& action);
 #endif
