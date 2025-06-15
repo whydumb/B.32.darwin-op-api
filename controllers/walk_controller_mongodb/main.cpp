97a4389427938b7878fbce22c421a30f217cd7e3
whydumb <108032052+whydumb@users.noreply.github.com>
2025-06-15 10:41:34 +0900

    Update main.cpp

--- controllers/walk_controller_mongodb/main.cpp
+++ controllers/walk_controller_mongodb/main.cpp
@@ -1,3 +1,4 @@
+// main.cpp - MongoDB 버전
 // Copyright 1996-2020 Cyberbotics Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,32 +13,184 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Description: MongoDB 실시간 명령 처리 컨트롤러 메인 함수
+// Description: Main entry point for MongoDB integrated ROBOTIS OP2 controller
 
-#include "walk_controller_mongodb.hpp"
+#include "walk_controller_mongodb.hpp"  // 헤더명 통일!
 
 #include <cstdlib>
 #include <iostream>
 
-using namespace webots;
 using namespace std;
 
 int main(int argc, char **argv) {
-    cout << "Starting ROBOTIS OP2 MongoDB Real-Time Controller..." << endl;
+    cout << "MongoDB 통합 ROBOTIS OP2 컨트롤러 시작..." << endl;
+    
+    Walk *controller = new Walk();
     
     try {
-        WalkMongoDB *controller = new WalkMongoDB();
         controller->run();
-        delete controller;
-    }
-    catch (const exception& e) {
-        cerr << "Error: " << e.what() << endl;
-        return EXIT_FAILURE;
-    }
-    catch (...) {
-        cerr << "Unknown error occurred" << endl;
-        return EXIT_FAILURE;
+    } catch (const exception& e) {
+        cout << "에러 발생: " << e.what() << endl;
     }
     
+    delete controller;
+    cout << "컨트롤러 종료" << endl;
+    
     return EXIT_SUCCESS;
-}
+}// main.cpp - MongoDB 버전
+// Copyright 1996-2020 Cyberbotics Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Description: Main entry point for MongoDB integrated ROBOTIS OP2 controller
+
+#include "walk_controller_mongodb.hpp"  // 헤더명 통일!
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "MongoDB 통합 ROBOTIS OP2 컨트롤러 시작..." << endl;
+    
+    Walk *controller = new Walk();
+    
+    try {
+        controller->run();
+    } catch (const exception& e) {
+        cout << "에러 발생: " << e.what() << endl;
+    }
+    
+    delete controller;
+    cout << "컨트롤러 종료" << endl;
+    
+    return EXIT_SUCCESS;
+}// main.cpp - MongoDB 버전
+// Copyright 1996-2020 Cyberbotics Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Description: Main entry point for MongoDB integrated ROBOTIS OP2 controller
+
+#include "walk_controller_mongodb.hpp"  // 헤더명 통일!
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "MongoDB 통합 ROBOTIS OP2 컨트롤러 시작..." << endl;
+    
+    Walk *controller = new Walk();
+    
+    try {
+        controller->run();
+    } catch (const exception& e) {
+        cout << "에러 발생: " << e.what() << endl;
+    }
+    
+    delete controller;
+    cout << "컨트롤러 종료" << endl;
+    
+    return EXIT_SUCCESS;
+}// main.cpp - MongoDB 버전
+// Copyright 1996-2020 Cyberbotics Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Description: Main entry point for MongoDB integrated ROBOTIS OP2 controller
+
+#include "walk_controller_mongodb.hpp"  // 헤더명 통일!
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "MongoDB 통합 ROBOTIS OP2 컨트롤러 시작..." << endl;
+    
+    Walk *controller = new Walk();
+    
+    try {
+        controller->run();
+    } catch (const exception& e) {
+        cout << "에러 발생: " << e.what() << endl;
+    }
+    
+    delete controller;
+    cout << "컨트롤러 종료" << endl;
+    
+    return EXIT_SUCCESS;
+}// main.cpp - MongoDB 버전
+// Copyright 1996-2020 Cyberbotics Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Description: Main entry point for MongoDB integrated ROBOTIS OP2 controller
+
+#include "walk_controller_mongodb.hpp"  // 헤더명 통일!
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "MongoDB 통합 ROBOTIS OP2 컨트롤러 시작..." << endl;
+    
+    Walk *controller = new Walk();
+    
+    try {
+        controller->run();
+    } catch (const exception& e) {
+        cout << "에러 발생: " << e.what() << endl;
+    }
+    
+    delete controller;
+    cout << "컨트롤러 종료" << endl;
+    
+    return EXIT_SUCCESS;
+}v
