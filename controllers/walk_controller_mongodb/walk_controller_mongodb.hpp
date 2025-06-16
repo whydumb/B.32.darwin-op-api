3d478f9ac31c6fd67a951e9a4a27ad6fd32027aa
whydumb <108032052+whydumb@users.noreply.github.com>
2025-06-16 12:34:25 +0900

    Update walk_controller_mongodb.hpp

--- controllers/walk_controller_mongodb/walk_controller_mongodb.hpp
+++ controllers/walk_controller_mongodb/walk_controller_mongodb.hpp
@@ -1,4 +1,4 @@
-// walk_controller_mongodb.hpp - 완전한 MongoDB 버전
+// walk_controller_mongodb.hpp - 개선된 MongoDB 버전
 #ifndef WALK_CONTROLLER_MONGODB_HPP
 #define WALK_CONTROLLER_MONGODB_HPP
 
@@ -10,7 +10,12 @@
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
 #include <mongocxx/collection.hpp>
+#include <mongocxx/database.hpp>
+#include <mongocxx/uri.hpp>
 #include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <bsoncxx/types.hpp>
+#include <bsoncxx/view_or_value.hpp>
 #include <memory>
 #include <string>
 #endif
@@ -48,13 +53,30 @@ private:
   managers::RobotisOp2GaitManager *mGaitManager;
 
 #ifdef USE_MONGODB
+  // MongoDB 인스턴스 및 연결
   std::unique_ptr<mongocxx::instance> mMongoInstance;
   std::unique_ptr<mongocxx::client> mMongoClient;
   mongocxx::collection mCollection;
+  
+  // 연결 상태 및 설정
   bool mMongoConnected;
   int mMongoCheckCounter;
+  std::string mLastAction;
+  int mReconnectAttempts;
+  static const int MAX_RECONNECT_ATTEMPTS = 5;
+  
+  // 설정값들
+  std::string mDbName;
+  std::string mCollectionName;
+  std::string mReplayName;
+  std::string mMongoUri;
+  int mPollInterval;
   
   // MongoDB 관련 메서드
+  void loadMongoConfig();
+  void initializeMongoDB();
+  void testMongoConnection();
+  bool reconnectMongoDB();
   std::string getMongoAction();
   void executeMongoAction(const std::string& action);
 #endif
